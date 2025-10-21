ce7ed034e72d8962e625f1ebd9cfae82d04dba5b
Brian Ojeda <9335829+sgtoj@users.noreply.github.com>
2025-10-21 11:41:07 -0400

    feat: add support for custom_key for rate based rules

--- main.tf
+++ main.tf
@@ -2565,6 +2565,108 @@ resource "aws_wafv2_web_acl" "main" {
               }
             }
 
+            dynamic "custom_key" {
+              for_each = lookup(rate_based_statement.value, "custom_keys", [])
+              content {
+                dynamic "cookie" {
+                  for_each = lookup(custom_key.value, "cookie", null) != null ? [lookup(custom_key.value, "cookie")] : []
+                  content {
+                    name = lookup(cookie.value, "name")
+                    dynamic "text_transformation" {
+                      for_each = lookup(cookie.value, "text_transformations", [])
+                      content {
+                        priority = lookup(text_transformation.value, "priority")
+                        type     = lookup(text_transformation.value, "type")
+                      }
+                    }
+                  }
+                }
+
+                dynamic "header" {
+                  for_each = lookup(custom_key.value, "header", null) != null ? [lookup(custom_key.value, "header")] : []
+                  content {
+                    name = lower(lookup(header.value, "name"))
+                    dynamic "text_transformation" {
+                      for_each = lookup(header.value, "text_transformations", [])
+                      content {
+                        priority = lookup(text_transformation.value, "priority")
+                        type     = lookup(text_transformation.value, "type")
+                      }
+                    }
+                  }
+                }
+
+                dynamic "http_method" {
+                  for_each = lookup(custom_key.value, "http_method", null) != null ? [lookup(custom_key.value, "http_method")] : []
+                  content {}
+                }
+
+                dynamic "ip" {
+                  for_each = lookup(custom_key.value, "ip", null) != null ? [lookup(custom_key.value, "ip")] : []
+                  content {}
+                }
+
+                dynamic "forwarded_ip" {
+                  for_each = lookup(custom_key.value, "forwarded_ip", null) != null ? [lookup(custom_key.value, "forwarded_ip")] : []
+                  content {}
+                }
+
+                dynamic "ja4_fingerprint" {
+                  for_each = lookup(custom_key.value, "ja4_fingerprint", null) != null ? [lookup(custom_key.value, "ja4_fingerprint")] : []
+                  content {
+                    fallback_behavior = lookup(ja4_fingerprint.value, "fallback_behavior")
+                  }
+                }
+
+                dynamic "label_namespace" {
+                  for_each = lookup(custom_key.value, "label_namespace", null) != null ? [lookup(custom_key.value, "label_namespace")] : []
+                  content {
+                    namespace = lookup(label_namespace.value, "namespace")
+                  }
+                }
+
+                dynamic "query_argument" {
+                  for_each = lookup(custom_key.value, "query_argument", null) != null ? [lookup(custom_key.value, "query_argument")] : []
+                  content {
+                    name = lookup(query_argument.value, "name")
+                    dynamic "text_transformation" {
+                      for_each = lookup(query_argument.value, "text_transformations", [])
+                      content {
+                        priority = lookup(text_transformation.value, "priority")
+                        type     = lookup(text_transformation.value, "type")
+                      }
+                    }
+                  }
+                }
+
+                dynamic "query_string" {
+                  for_each = lookup(custom_key.value, "query_string", null) != null ? [lookup(custom_key.value, "query_string")] : []
+                  content {
+                    dynamic "text_transformation" {
+                      for_each = lookup(query_string.value, "text_transformations", [])
+                      content {
+                        priority = lookup(text_transformation.value, "priority")
+                        type     = lookup(text_transformation.value, "type")
+                      }
+                    }
+                  }
+                }
+
+                dynamic "uri_path" {
+                  for_each = lookup(custom_key.value, "uri_path", null) != null ? [lookup(custom_key.value, "uri_path")] : []
+                  content {
+                    dynamic "text_transformation" {
+                      for_each = lookup(uri_path.value, "text_transformations", [])
+                      content {
+                        priority = lookup(text_transformation.value, "priority")
+                        type     = lookup(text_transformation.value, "type")
+                      }
+                    }
+                  }
+                }
+              }
+            }
+
             dynamic "scope_down_statement" {
               for_each = length(lookup(rate_based_statement.value, "scope_down_statement", {})) == 0 ? [] : [lookup(rate_based_statement.value, "scope_down_statement", {})]
               content {
